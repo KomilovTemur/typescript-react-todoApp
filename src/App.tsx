3b1b75b22808d95bb43d60097b41b0c7dc67237c
Temurbek Komilov <itteamdev74@gmail.com>
2023-05-18 10:34:01 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const App: FC = () => {
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
   const [todoList, setTodoList] = useState<ITask[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
     if (e.target.name === "task") {
@@ -17,10 +18,16 @@ const App: FC = () => {
   };
 
   const addTask = (): void => {
+    if (task === "") {
+      return setError("Task is required");
+    } else if (deadline < 0) {
+      return setError("As long as time is greater than 0");
+    }
     const newTask = { taskName: task, deadline };
     setTodoList([...todoList, newTask]);
     setTask("");
     setDeadline(0);
+    setError(null);
   };
 
   const completeTask = (taskNameToDelete: string): void => {
@@ -33,6 +40,14 @@ const App: FC = () => {
 
   return (
     <div className="App">
+      {error && (
+        <div className="errorDialog">
+          <p className="dialog">{error}</p>
+          <button onClick={() => setError(null)} className="closeErrorDialog">
+            X
+          </button>
+        </div>
+      )}
       <div className="header">
         <div className="inputContainer">
           <input
@@ -53,9 +68,11 @@ const App: FC = () => {
         <button onClick={addTask}>Add Task</button>
       </div>
       <div className="todoList">
-        {todoList.map((task: ITask, key: number) => (
-          <TodoTask key={key} task={task} completeTask={completeTask} />
-        ))}
+        {todoList.length == 0
+          ? "there is no tasks yet"
+          : todoList.map((task: ITask, key: number) => (
+              <TodoTask key={key} task={task} completeTask={completeTask} />
+            ))}
       </div>
     </div>
   );
